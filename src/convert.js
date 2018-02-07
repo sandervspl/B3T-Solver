32210283a61388e50772d4930d4803c59dd0edf9
Sander Vispoel <sandervispoel@hotmail.com>
2018-02-07 18:46:05 +0100

    Add convert ascii chars to single chars

--- src/convert.js
+++ src/convert.js
@@ -1,13 +1,21 @@
+import { flatten } from 'lodash';
+
 const hashToAscii = (hash) => {
     return hash
         .split('')
         .map(char => isNaN(char) ? char.charCodeAt(0).toString() : char);
 };
 
-const convert = (hash) => {
-    const ascii = hashToAscii(hash);
+const splitAsciiChars = (ascii) => {
+    return flatten(ascii.map(char => char.split('')));
+};
 
-    return ascii;
+const convert = (hash) => {
+    let binary;
+    binary = hashToAscii(hash);
+    binary = splitAsciiChars(binary);
+
+    return binary;
 };
 
 export default convert;
