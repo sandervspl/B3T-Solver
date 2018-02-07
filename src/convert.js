0ab043d7856067768d7cdf1a2d9de77475079ea1
Sander Vispoel <sandervispoel@hotmail.com>
2018-02-07 19:29:26 +0100

    Update ascii array length to multiple of 10

--- src/convert.js
+++ src/convert.js
@@ -1,4 +1,4 @@
-import { flatten } from 'lodash';
+import { flatten, times } from 'lodash';
 
 const hashToAscii = (hash) => {
     return hash
@@ -10,10 +10,29 @@ const splitAsciiChars = (ascii) => {
     return flatten(ascii.map(char => char.split('')));
 };
 
+const toMultipleOfTen = (array) => {
+    console.log('cur length:', array.length);
+
+    if (array.length % 10 !== 0) {
+        const diff = 10 - array.length % 10;
+        let i = 0;
+
+        times(
+            diff,
+            () => array.push((i++).toString())
+        );
+
+        console.log('new length:', array.length);
+    }
+
+    return array;
+};
+
 const convert = (hash) => {
     let binary;
     binary = hashToAscii(hash);
     binary = splitAsciiChars(binary);
+    binary = toMultipleOfTen(binary);
 
     return binary;
 };
