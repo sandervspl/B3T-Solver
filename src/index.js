c807cd8e6b35efbfa64a36adb21f767a5ace2792
Sander Vispoel <sandervispoel@hotmail.com>
2018-02-07 19:29:01 +0100

    Start app on server start

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ app.set('port', 3000);
 
 app.listen(app.get('port'), () => {
     console.log(`Server started on port ${app.get('port')}`);
-});
 
-const binary = convertHashToBinary(hash);
-console.log(binary);
+    console.log(`Hash is: ${hash}`);
+    const binary = convertHashToBinary(hash);
+    console.log(binary);
+});
