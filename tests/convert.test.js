8e631cb4f3afcfb948f586446dca3ddb4fb5296f
Sander Vispoel <sandervispoel@hotmail.com>
2018-02-10 17:41:20 +0100

    Add new tests for toMultipleOfTen

--- tests/convert.test.js
+++ tests/convert.test.js
@@ -1,10 +1,17 @@
 import {
     hashToAscii,
     splitAsciiChars,
+    toMultipleOfTen,
 } from '../src/convert';
 
 describe('convert', () => {
     let hash;
+
+    const compareArrays = (a1, a2) => {
+        a1.forEach((num, i) => {
+            expect(num).toEqual(a2[i]);
+        });
+    };
 
     beforeEach(() => {
         hash = 'text';
@@ -16,12 +23,6 @@ describe('convert', () => {
         beforeEach(() => {
             hashAscii = ['116', '101', '120', '116'];
         });
-
-        const compareArrays = (a1, a2) => {
-            a1.forEach((num, i) => {
-                expect(num).toEqual(a2[i]);
-            });
-        };
 
         it('Returns an array of strings', () => {
             const asciiArray = hashToAscii(hash);
@@ -67,6 +68,34 @@ describe('convert', () => {
         it('Returns an array of single characters', () => {
             splitArray.forEach((str) => {
                 expect(str.length).toEqual(1);
+            });
+        });
+
+        it('Correctly splits an array of ASCII codes', () => {
+            const answer = ['1', '1', '6', '1', '0', '1', '1', '2', '0', '1', '1', '6'];
+            compareArrays(splitArray, answer);
+        });
+    });
+
+    describe('toMultipleOfTen()', () => {
+        let array;
+
+        beforeEach(() => {
+            array = ['0', '1', '2', '3'];
+        });
+
+        it('Adds digits to an array until it is a multiple of ten', () => {
+            expect(toMultipleOfTen(array).length % 10).toEqual(0);
+        });
+
+        it('Adds numbers 0 to 9 to the array', () => {
+            const offset = array.length % 10;
+            const newArray = toMultipleOfTen(array);
+
+            newArray.forEach((num, i) => {
+                if (i < offset) return;
+                const j = (i - offset) + '';
+                expect(num).toEqual(j);
             });
         });
     });
